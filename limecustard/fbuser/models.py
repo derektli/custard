a8eef81e067620d38b9fd828c6bd5e8019f26d28
formatjam <formatjam@gmail.com>
2013-04-03 21:58:09 -0400

    fixed model stupid typo bugs. Thanks to jason

--- limecustard/fbuser/models.py
+++ limecustard/fbuser/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
-    def _unicode_(self):
+    def __unicode__(self):
         return self.title
 
 class User(models.Model):
